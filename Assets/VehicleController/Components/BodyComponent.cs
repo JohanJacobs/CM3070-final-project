73814dc6dd55b945dc3e3183ae4ec079ed75b70c
Johan Jacobs <20131902+JohanJacobs@users.noreply.github.com>
2025-06-22 10:05:51 +1200

    VehicleDrag

--- Assets/VehicleController/Components/BodyComponent.cs
+++ Assets/VehicleController/Components/BodyComponent.cs
@@ -15,19 +15,23 @@ namespace vc
             Transform leftWheel;
             Transform rightWheel;
 
-            float wheelBaseLength;
-            float turnRadius;
-            float wheelBaseRearTrackLength;
+            float bodyDragCoefficient = 0.34f; // constant 
 
-            float ackermanAngleLeft = 0f;
-            float ackermanAngleRight = 0f;
+            //http://www.mayfco.com/dragcd~1.htm
+            float bodyArea = 1.74f; // m² 
 
+            float wheelBaseLength; // m
+            float turnRadius;      // m
+            float wheelBaseRearTrackLength; // m
 
             float aeroDrag => 0f; // TODO:
-            float bodyDrag => 0f; // TODO:
+            float bodyDrag => MathHelper.CalculateDrag(velocityLS.z,bodyArea,bodyDragCoefficient); // nm TODO:
             public float DragForce => aeroDrag + bodyDrag;
             public float mass => 1500; // kg
-
+            public float MStoKMH = 3.6f; //constant
+            public float SpeedKMH =>velocityLS.z * MStoKMH; // Km/H
+            Vector3 velocityWS => rb.velocity; // MS
+            Vector3 velocityLS => rb.transform.InverseTransformDirection(velocityWS); //MS
 
 
             public BodyComponent( BodySO config, Rigidbody rb, Transform leftWheel, Transform rightWheel)
@@ -59,16 +63,17 @@ namespace vc
             public void Update(float dt)
             {
                 UpdateAckermanSteering();
+                AddBodyDragForce(dt);
             }
             #endregion IVehicleComponent
 
             #region bodycomponent
             void UpdateAckermanSteering()
             {
-
                 float steerValue = steerInput.Value;
+                float ackermanAngleLeft = 0f;
+                float ackermanAngleRight = 0f;
 
-                
                 // calculate angle in degrees 
                 if (steerValue < 0f)
                 {
@@ -86,6 +91,13 @@ namespace vc
                 rightWheel.localRotation = Quaternion.Euler(new Vector3(rightWheel.localRotation.x, ackermanAngleLeft, rightWheel.localRotation.z));
             }
 
+            void AddBodyDragForce(float dt)
+            {
+                rb.AddForce(-rb.transform.forward * bodyDrag);
+            }
+            #endregion bodycomponent
+
+            #region IDebugInformation
             public void DrawGizmos()
             {
                 
@@ -94,11 +106,14 @@ namespace vc
             public float OnGUI(float xOffset, float yOffset, float yStep)
             {
                 GUI.Label(new Rect(xOffset, yOffset += yStep, 200f, yStep), $"- BODY:");
-                GUI.Label(new Rect(xOffset, yOffset += yStep, 200f, yStep), $"  Velo :{rb.transform.InverseTransformDirection(rb.velocity)}");
+                GUI.Label(new Rect(xOffset, yOffset += yStep, 200f, yStep), $"  Km/h :{SpeedKMH.ToString("F0")}");
+                GUI.Label(new Rect(xOffset, yOffset += yStep, 200f, yStep), $"  Velo :{velocityLS.ToString("f2")}");
+                GUI.Label(new Rect(xOffset, yOffset += yStep, 200f, yStep), $"  B.Drag :{bodyDrag.ToString("f2")}");
+
 
                 return yOffset;
             }
-            #endregion bodycomponent
+            #endregion IDebugInformation
         }
     }
 }
--- Assets/VehicleController/MathHelper/MathHelper.cs
+++ Assets/VehicleController/MathHelper/MathHelper.cs
@@ -39,6 +39,16 @@ public class MathHelper
             return 0f;
         return a / b;
     }
-       
 
+    // CalculateDragForce 
+    // air density : kg/m³
+    // area : m²
+    // velocity : ms
+    // coefficient of drag : constant
+    // https://www.grc.nasa.gov/www/k-12/VirtualAero/BottleRocket/airplane/drageq.html
+    //// typical values is 0.2 to 0.4 //https://en.wikipedia.org/wiki/Automobile_drag_coefficient#:~:text=The%20average%20modern%20automobile%20achieves,=0.285%20to%20Cd=0.315.
+    public static float CalculateDrag(float velocityMS, float areaM, float dragCoefficient, float airDensity=1.225f)
+    {
+        return 0.5f * airDensity * (velocityMS * velocityMS) * areaM * dragCoefficient;
+    }
 }
