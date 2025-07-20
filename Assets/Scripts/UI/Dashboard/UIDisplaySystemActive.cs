accf95a184c1bdb53bd46903a3a5e1b34ad1d086
Johan Jacobs <20131902+JohanJacobs@users.noreply.github.com>
2025-07-20 18:29:22 +1200

    TC feedback in Dashboard UI

--- Assets/Scripts/UI/Dashboard/UIDisplaySystemActive.cs
+++ Assets/Scripts/UI/Dashboard/UIDisplaySystemActive.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class UIDisplayABSActive : MonoBehaviour
+public class UIDisplaySystemActive : MonoBehaviour
 {
     [SerializeField] Color ActiveColor;
     [SerializeField] Color inactiveColor;
-    [SerializeField] BoolVariable absVariable;
+    [SerializeField] BoolVariable Variable;
     [SerializeField] TextMeshProUGUI text;
 
-    Color GetTextColor => absVariable.Value ? ActiveColor : inactiveColor;
+    Color GetTextColor => Variable.Value ? ActiveColor : inactiveColor;
     public void Update()
     {
         text.color = GetTextColor;
