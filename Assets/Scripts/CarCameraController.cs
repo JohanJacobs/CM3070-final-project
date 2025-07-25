1231eb82d8b8eef02987db82b67298f676b2c9fd
Johan Jacobs <20131902+JohanJacobs@users.noreply.github.com>
2025-07-26 11:09:17 +1200

    new camera controller

---
+++ Assets/Scripts/CarCameraController.cs
@@ -0,0 +1,26 @@
+using Sirenix.OdinInspector.Editor.TypeSearch;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarCameraController : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] Rigidbody rb;
+    [SerializeField] float speed;
+    [SerializeField] Vector3 offset;
+
+    // Camera Stutters at speed 
+    Vector3 targetforward => (target.forward + rb.velocity).normalized;
+    private void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(
+            transform.position, 
+            target.position + target.TransformVector(offset) + targetforward * (-5f),
+            speed * Time.deltaTime);
+
+        transform.LookAt(target);
+    }
+
+    
+}
