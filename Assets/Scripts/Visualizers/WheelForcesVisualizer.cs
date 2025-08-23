858eeccf4fb38f61c4a7ed84e89a07056cd4e65a
Johan Jacobs <20131902+JohanJacobs@users.noreply.github.com>
2025-08-23 23:51:41 +1200

    fixed Normal Force Visualization

--- Assets/Scripts/Visualizers/WheelForcesVisualizer.cs
+++ Assets/Scripts/Visualizers/WheelForcesVisualizer.cs
@@ -8,20 +8,34 @@ namespace VC
 {
     public class WheelForcesVisualizer : MonoBehaviour, IToggle
     {
-        [SerializeField] Transform carBody;
+        [Header("?Settings")]
+        [SerializeField] bool ShowSpringCompression=false;
+
+        [Header("Configuration")]
+        [SerializeField] Transform[] hideBodyParts;
         [SerializeField] MeshRenderer frontRightMeshRenderer;
         [SerializeField] MeshRenderer frontLeftMeshRenderer;
         [SerializeField] MeshRenderer rearRightMeshRenderer;
         [SerializeField] MeshRenderer rearLeftMeshRenderer;
+        [SerializeField] MeshRenderer leftFrontNormalForceCylinder;
+        [SerializeField] MeshRenderer rightFrontNormalForceCylinder;
+        [SerializeField] MeshRenderer leftBackNormalForceCylinder;
+        [SerializeField] MeshRenderer rightBackNormalForceCylinder;
+
 
         Dictionary<WheelID, WheelVisualData> wheelData;
 
+
+        [SerializeField] float maximumNormalForce = 0f;
         private class WheelVisualData
         {
             public WheelHitData wheelData;
             public Vector3 previousLocalScale = default;
             public Material material;
             public MeshRenderer meshRenderer;
+
+            public MeshRenderer normalForceMeshRenderer;
+            public Material normalForceMaterial;
         }
 
         private void Awake()
@@ -34,7 +48,14 @@ namespace VC
             frontRightMeshRenderer.enabled = false;
             frontLeftMeshRenderer.enabled = false;
             rearRightMeshRenderer.enabled = false;
-            rearLeftMeshRenderer.enabled = false;                       
+            rearLeftMeshRenderer.enabled = false;
+
+            leftFrontNormalForceCylinder.enabled = false;
+            rightFrontNormalForceCylinder.enabled = false;
+            leftBackNormalForceCylinder.enabled = false;
+            rightBackNormalForceCylinder.enabled = false;
+
+
         }
 
         private void OnEnable()
@@ -54,14 +75,15 @@ namespace VC
             UpdateVisual(WheelID.RightRear);
             UpdateVisual(WheelID.LeftRear);
         }
-
         void UpdateVisual(WheelID id)
         {
             if (!wheelData.ContainsKey(id)) return;
 
+
+            // Update Lateral and Longitudinal Forces 
             var visualData = wheelData[id];
             var data = visualData.wheelData;
-
+            // normalize the forces
             var localScale = new Vector3(
                 data.combinedSlip.y * data.normalforce, // X - lateral  forces - left right
                 data.normalforce,                       // Y - normal forces - up down
@@ -70,16 +92,43 @@ namespace VC
 
             visualData.previousLocalScale = Vector3.Lerp(visualData.previousLocalScale, localScale, Time.deltaTime);
 
+            // Position the visual for longitudinal and lateral forces accounting for the movement of the wheel
             visualData.meshRenderer.transform.position = data.hitInfo.point + data.suspensionMountPoint.up * 0.05f;
+
+            // Set the shader parameters
             visualData.material.SetVector("_ForcePosition", new Vector4(localScale.x, localScale.z));
+
+            // Update shader for Normal Forces visual
+            visualData.normalForceMaterial.SetFloat("_ForcePercentage",
+                ShowSpringCompression?data.suspension.springCompression: data.normalforce / maximumNormalForce);
         }
         MeshRenderer GetMeshRenderer(WheelID id)
         {
-            if (id == WheelID.LeftFront) return frontLeftMeshRenderer;
-            if (id == WheelID.RightFront) return frontRightMeshRenderer;
-            if (id == WheelID.LeftRear) return rearLeftMeshRenderer;
-            if (id == WheelID.RightRear) return rearRightMeshRenderer;
-            return null;
+            switch (id)
+            {
+                case WheelID.LeftRear: return rearLeftMeshRenderer;
+                case WheelID.RightRear: return rearRightMeshRenderer;
+                case WheelID.RightFront: return frontRightMeshRenderer;
+                case WheelID.LeftFront: return frontLeftMeshRenderer;
+                default:return null;
+            }
+            //if (id == WheelID.LeftFront) return frontLeftMeshRenderer;
+            //if (id == WheelID.RightFront) return frontRightMeshRenderer;
+            //if (id == WheelID.LeftRear) return rearLeftMeshRenderer;
+            //if (id == WheelID.RightRear) return rearRightMeshRenderer;
+            //return null;
+        }
+
+        MeshRenderer GetCylinderMeshRenderer(WheelID id)
+        {
+            switch(id)
+            {
+                case WheelID.LeftRear: return leftBackNormalForceCylinder;
+                case WheelID.RightRear: return rightBackNormalForceCylinder;
+                case WheelID.LeftFront: return leftFrontNormalForceCylinder;
+                case WheelID.RightFront: return rightFrontNormalForceCylinder;
+                default: return null;
+            }
         }
 
         void WheelComponent_onVisualWheelUpdate(WheelID id, WheelHitData data)
@@ -87,13 +136,17 @@ namespace VC
             if (!wheelData.ContainsKey(id))
             {
                 var wheelMeshRenderer = GetMeshRenderer(id);
+                var wheelNormalForceMeshRenderer = GetCylinderMeshRenderer(id);
+
                 wheelData.Add(id, new WheelVisualData
                 {
                     wheelData = data,
                     previousLocalScale = new Vector3(),
                     meshRenderer = wheelMeshRenderer,
-                    material = wheelMeshRenderer.material
-                });
+                    material = wheelMeshRenderer.material,
+                    normalForceMeshRenderer = wheelNormalForceMeshRenderer,
+                    normalForceMaterial = wheelNormalForceMeshRenderer.material,
+                });                                
                 return;
             }
         }
@@ -104,11 +157,13 @@ namespace VC
         }
         void ToggleVisuals()
         {
-            carBody.gameObject.SetActive(!carBody.gameObject.activeSelf);
+
+            hideBodyParts.ForEach( p => p.gameObject.SetActive(!p.gameObject.activeSelf));
 
             wheelData.ForEach(wheel =>
             {
                 wheel.Value.meshRenderer.enabled = !wheel.Value.meshRenderer.enabled;
+                wheel.Value.normalForceMeshRenderer.enabled = !wheel.Value.normalForceMeshRenderer.enabled;
             });
         }
     }
